1fb409ba88a7d12594dfbf3798e4c10c6661ccff
alizhan <alizhan.abdulkhalil@inbox.ru>
2019-10-16 19:22:51 +0600

    a..z

--- printalphabet/printalphabet.go
+++ printalphabet/printalphabet.go
@@ -2,9 +2,10 @@ package main
 
 import "github.com/01-edu/z01"
 
-func main(){
- for i := 97; i<=122; i++{
-                z01.PrintRune(i)
-        }
-println()
+func main() {
+       for i := 97; i <= 122; i++ {
+               z01.PrintRune(i)
+       }
+       z01.PrintRune(92110)
 }
+
