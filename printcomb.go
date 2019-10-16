f86806ddbecbda6779065e3699f15be951a63447
alizhan <alizhan.abdulkhalil@inbox.ru>
2019-10-16 21:15:39 +0600

    3 numbers

---
+++ printcomb.go
@@ -0,0 +1,16 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			for l := '0'; l <= '9'; l++ {
+				if i < j && j < l {
+					z01.PrintRune(i, j, l, ", ")
+				}
+			}
+		}
+
+	}
+}
