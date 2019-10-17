b751db361041a90413013934c1526dc515ec8c3a
alizhan <alizhan.abdulkhalil@inbox.ru>
2019-10-17 19:35:16 +0600

    tralebus2

--- basicatoi2.go
+++ basicatoi2.go
@@ -5,12 +5,12 @@ func BasicAtoi2(s string) int {
 	var a int = 0
 
 	for _, num := range s {
+		var numb int = 0
 		if num > '9' || num < '0' {
-			numb == '0'
+			numb == 0
 			break
 		}
 
-		var numb int = 0
 		for i := '1'; i <= num; i++ {
 			numb++
 		}
