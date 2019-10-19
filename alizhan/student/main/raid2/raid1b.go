0645293cdfe3b4e8483de416c56133934d6eec0a
alizhan <alizhan.abdulkhalil@inbox.ru>
2019-10-19 13:29:31 +0600

    raid1b.go

---
+++ alizhan/student/main/raid2/raid1b.go
@@ -0,0 +1,31 @@
+package piscine 
+
+import "github.com/01-edu/z01"
+
+func Raid1b(x, y int) {
+
+	for i := 0; i < y; i++ {
+
+		for j := 0; j < x; j++ {
+
+			if (i == 0 || i == (y - 1)) && (j == 0 || (j == (x - 1))) {
+				if (i == 0 && j == 0) || (j == (x - 1) && i == (y - 1)) {
+					z01.PrintRune('/')
+				} else if (i == (y - 1) && j == 0) || (i == 0 && j == (x - 1)) {
+					z01.PrintRune(92)
+				} 
+			} else if (i == 0 || i == (y - 1)) && (j != 0 || (j != (x - 1))) {
+				z01.PrintRune('*')
+			} else if (i != 0 || i != (y - 1)) && (j == 0 || (j == x - 1)) {
+				z01.PrintRune('*')
+			} else if (i != 0 || i != (y - 1)) && (j != 0 || (j != x - 1)) {
+				z01.PrintRune(' ')
+			}
+
+		}
+	z01.PrintRune(10)
+
+
+	}
+
+}
