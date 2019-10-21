f70e81aeb32b4df660efaed0787e25e26318f114
alizhan <alizhan.abdulkhalil@inbox.ru>
2019-10-21 18:16:56 +0600

    m

---
+++ iterativefactorial.go
@@ -0,0 +1,23 @@
+package piscine
+
+import (
+	"fmt"
+)
+
+func IterativeFactorial(nb int) int {
+
+	f := 0
+	if nb > 0 {
+
+		for i := 0; i <= nb; i++ {
+
+			f = f + i
+
+		}
+
+		fmt.print(f)
+
+	} else {
+		return 0
+	}
+}
