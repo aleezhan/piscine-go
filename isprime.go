b334779b25d6417d0866ea5d000ef01d4222626a
alizhan <alizhan.abdulkhalil@inbox.ru>
2019-10-21 20:08:17 +0600

    al

--- isprime.go
+++ isprime.go
@@ -1,10 +1,10 @@
 package piscine
 
 func IsPrime(nb int) bool {
-	if nb == 2 || nb == 3 || nb == 5 {
-		return false
+	if nb == 1 {
+		return true
 	}
-	if nb%2 != 0 || nb%3 != 0 || nb%5 != 0 {
+	if nb%2 != 0 && nb%3 != 0 && nb%5 != 0 {
 		return true
 	}
 	return false
