ab4d619bb7ee6ed763454c58f5f7217a7bf85d6c
alizhan <alizhan.abdulkhalil@inbox.ru>
2019-10-21 20:21:53 +0600

    ld

--- findnextprime.go
+++ findnextprime.go
@@ -1,19 +1,23 @@
 package piscine
 
 func FindNextPrime(nb int) int {
+	a := 0
 	for i := nb; i < nb+1000; i++ {
 		if i == 2 || i == 3 || i == 5 {
-			a := i
+			a = i
 			break
 		}
 		if i == 1 {
-			a := i
+			a = i
 			break
 		}
 		if i%2 != 0 && i%3 != 0 && i%5 != 0 {
-			a := i
+			a = i
 			break
 		}
+	}
+	if a == 0 {
+		return
 	}
 	return a
 }
