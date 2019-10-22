86579c2ca94c82722ae3748687c8b949ad97697b
alizhan <alizhan.abdulkhalil@inbox.ru>
2019-10-22 20:58:26 +0600

    dkfj

--- compare.go
+++ compare.go
@@ -4,23 +4,8 @@ func Compare(a, b string) int {
 	if a == b {
 		return 0
 	}
-	ar := []rune(a)
-	arr := []rune(b)
-	indexx := 0
-	c := 0
-	for _, lett := range b {
-		lett = lett
-		indexx++
+	if a < b {
+		return -1
 	}
-	for i := 0; i < indexx; i++ {
-		if ar[0] == arr[0] {
-			continue
-		}
-		c++
-		break
-	}
-	if c == 0 {
-		return 1
-	}
-	return -1
+	return 1
 }
