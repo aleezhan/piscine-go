101d1835fb60352676002e02c8ecd16ef5f43fc9
alizhan <alizhan.abdulkhalil@inbox.ru>
2019-10-22 21:24:57 +0600

    asd

--- capitalize.go
+++ capitalize.go
@@ -5,9 +5,9 @@ func Capitalize(s string) string {
 	l := 0
 	l = l
 	if arr[0] >= 97 && arr[0] <= 122 {
-		l := 0
+		l = 0
 	} else {
-		l := 1
+		l = 1
 	}
 	for index, letter := range s {
 		if letter >= 97 && letter <= 122 && l == 0 {
