665f9cef0047606f6fe8db3315b1119589d49d78
alizhan <alizhan.abdulkhalil@inbox.ru>
2019-10-22 23:17:30 +0600

    sdf

--- findnextprime.go
+++ findnextprime.go
@@ -1,39 +1,22 @@
 package piscine
 
+func check(x int) bool {
+	if x <= 1 {
+		return false
+	}
+	for i := 2; i*i <= x; i++ {
+		if x%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
 func FindNextPrime(nb int) int {
-	a := 0
-	if nb < 0 {
-		nb = 0
+	for {
+		if check(nb) {
+			return nb
+		}
+		nb++
 	}
-	for i := nb; i < nb+1000; i++ {
-		d := 0
-		if i == 0 {
-			continue
-		}
-		for j := 0; j < i; j++ {
-			if i%j == 0 {
-				d++
-				break
-			}
-		}
-		if d > 0 {
-			continue
-		}
-		if i == 1 {
-			a = i + 1
-			break
-		}
-		if i == 2 || i == 3 || i == 5 || i == 7 || i == 11 {
-			a = i
-			break
-		}
-		if i%2 != 0 && i%3 != 0 && i%5 != 0 && i%7 != 0 && i%11 != 0 {
-			a = i
-			break
-		}
-	}
-	if a == 0 {
-		return a
-	}
-	return a
 }
