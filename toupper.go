2507f199f751590617bdf1b204fa999b872c04f2
alizhan <alizhan.abdulkhalil@inbox.ru>
2019-10-22 21:06:25 +0600

    ksdjf

--- toupper.go
+++ toupper.go
@@ -1,10 +1,11 @@
 package piscine
 
 func ToUpper(s string) string {
-	for _, letter := range s {
+	arr := []rune(s)
+	for index, letter := range s {
 		if letter >= 97 && letter <= 122 {
-			letter -= 32
+			arr[index] = letter - 32
 		}
 	}
-	return s
+	return arr
 }
