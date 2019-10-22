81cbfaf9b9c4100004eb27adbd3bf9e202b71877
alizhan <alizhan.abdulkhalil@inbox.ru>
2019-10-22 20:16:31 +0600

    skdf

--- index.go
+++ index.go
@@ -3,12 +3,13 @@ package piscine
 func Index(s string, toFind string) int {
 	index := 0
 	arr := []rune(toFind)
+	arr2 := []rune(s)
 	for in, letter := range s {
 		if arr[0] == letter {
 			index = in
-			for i := 0; i < toFind.length(); i++ {
-				if arr[i] == letter+i {
-					if i == toFind.length() {
+			for i := 0; arr[i] != '\x00'; i++ {
+				if arr[i] == arr2[in+i] {
+					if i+1 == '\x00' {
 						return index
 					}
 				}
