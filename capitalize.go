5f8257135c12d20b265f16161caff22ae01409a3
alizhan <alizhan.abdulkhalil@inbox.ru>
2019-10-22 22:12:42 +0600

    asd

--- capitalize.go
+++ capitalize.go
@@ -5,35 +5,34 @@ func Capitalize(s string) string {
 	if my_arr[0] >= 'a' && my_arr[0] <= 'z' {
 		my_arr[0] = rune(my_arr[0] - 32)
 	}
+
 	k := 0
-	for _, let := range s {
-		let = let
-		k++
+
+	for index := range my_arr {
+		k = index
 	}
-	for index, letter := range s {
-		letter = letter
-		if k-2 == index {
-			continue
+
+	for i := 1; i <= k; i++ {
+		if (my_arr[i-1] < 'A' || my_arr[i-1] > 'Z') &&
+			(my_arr[i] >= 'a' && my_arr[i] <= 'z') &&
+			(my_arr[i-1] < 'a' || my_arr[i-1] > 'z') &&
+			(my_arr[i-1] < '0' || my_arr[i-1] > '9') {
+			my_arr[i] = rune(my_arr[i] - 32)
 		}
-		if (my_arr[index] < 'A' || my_arr[index] > 'Z') &&
-			(my_arr[index+1] >= 'a' && my_arr[index+1] <= 'z') &&
-			(my_arr[index] < 'a' || my_arr[index] > 'z') &&
-			(my_arr[index] < '0' || my_arr[index] > '9') {
-			my_arr[index+1] = rune(my_arr[index+1] - 32)
+		if (my_arr[i-1] >= 'A' && my_arr[i-1] <= 'Z') &&
+			(my_arr[i] >= 'A' && my_arr[i] <= 'Z') {
+			my_arr[i] = rune(my_arr[i] + 32)
 		}
-		if (my_arr[index] >= 'A' && my_arr[index] <= 'Z') &&
-			(my_arr[index+1] >= 'A' && my_arr[index+1] <= 'Z') {
-			my_arr[index+1] = rune(my_arr[index+1] + 32)
+		if (my_arr[i-1] >= 'a' && my_arr[i-1] <= 'z') &&
+			(my_arr[i] >= 'A' && my_arr[i] <= 'Z') {
+			my_arr[i] = rune(my_arr[i] + 32)
 		}
-		if (my_arr[index] >= 'a' && my_arr[index] <= 'z') &&
-			(my_arr[index+1] >= 'A' && my_arr[index+1] <= 'Z') {
-			my_arr[index+1] = rune(my_arr[index+1] + 32)
-		}
-		if (my_arr[index] >= '0' && my_arr[index] <= '9') &&
-			(my_arr[index+1] >= 'A' && my_arr[index+1] <= 'Z') {
-			my_arr[index+1] = rune(my_arr[index+1] + 32)
+		if (my_arr[i-1] >= '0' && my_arr[i-1] <= '9') &&
+			(my_arr[i] >= 'A' && my_arr[i] <= 'Z') {
+			my_arr[i] = rune(my_arr[i] + 32)
 		}
 
 	}
+
 	return string(my_arr)
 }
