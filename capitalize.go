d7815af0ba95073a773cb37e16602906cb474c1e
alizhan <alizhan.abdulkhalil@inbox.ru>
2019-10-22 21:36:28 +0600

    asd

--- capitalize.go
+++ capitalize.go
@@ -2,11 +2,6 @@ package piscine
 
 func Capitalize(s string) string {
 	arr := []rune(s)
-	k := 0
-	for _, let := range s {
-		let = let
-		k++
-	}
 	l := 0
 	l = l
 	if arr[0] >= 97 && arr[0] <= 122 {
@@ -18,6 +13,9 @@ func Capitalize(s string) string {
 		if letter >= 97 && letter <= 122 && l == 0 {
 			arr[index] = letter - 32
 			l++
+		}
+		if letter < 97 || letter > 122 {
+			l--
 		}
 
 	}
