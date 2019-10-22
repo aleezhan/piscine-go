01b718de2cf9d515f46594c25c70d5cfb0d1ad89
alizhan <alizhan.abdulkhalil@inbox.ru>
2019-10-22 17:51:58 +0600

    d

--- alphacount.go
+++ alphacount.go
@@ -2,7 +2,7 @@ package piscine
 
 func AlphaCount(str string) int {
 	counter := 0
-	for index, letter := range str {
+	for _, letter := range str {
 		if (letter >= 65 && letter <= 90) || (letter >= 97 && letter <= 172) {
 			counter++
 		}
