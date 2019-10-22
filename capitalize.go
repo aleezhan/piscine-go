027979f2c3f52840b1177e4275331faf35355dca
alizhan <alizhan.abdulkhalil@inbox.ru>
2019-10-22 21:22:14 +0600

    asd

---
+++ capitalize.go
@@ -0,0 +1,21 @@
+package piscine
+
+func Capitalize(s string) string {
+	arr := []rune(s)
+	if arr[0] >= 97 && arr[0] <= 122 {
+		l := 0
+	} else {
+		l := 1
+	}
+	for index, letter := range s {
+		if letter >= 97 && letter <= 122 && l == 0 {
+			arr[index] = letter - 32
+			l++
+		}
+		if (letter < 97 || letter > 122) && (arr[index+1] >= 97 && arr[index+1] <= 122) {
+			l--
+		}
+
+	}
+	return string(arr)
+}
