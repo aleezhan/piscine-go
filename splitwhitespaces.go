dc7066c193055111de9ff018255d4e2baf525cbf
alizhan <alizhan.abdulkhalil@inbox.ru>
2019-10-25 06:57:08 +0600

    sdfjk

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -3,18 +3,21 @@ package piscine
 func SplitWhiteSpaces(str string) []string {
 	ind := 1
 	for _, word := range str {
-		if word == ' ' {
+		if word == ' ' || word == '\n' || word == '\t' {
 			ind++
 		}
 	}
 	strr := make([]string, ind)
 	in := 0
+	b := 0
 	for _, words := range str {
-		if words == ' ' {
+		if words == ' ' && b == 0 {
 			in++
+			b++
 			continue
 		}
 		strr[in] += string(words)
+		b = 0
 	}
 	return strr
 
